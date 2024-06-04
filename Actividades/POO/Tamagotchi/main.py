190daf67006eb136b2c03855c5ad46476fe7fc63
GMSanchezBattagion <lalo200911@gmail.com>
2024-06-04 15:59:25 -0300

    Se corrige ejercicio POO

--- Actividades/POO/Tamagotchi/main.py
+++ Actividades/POO/Tamagotchi/main.py
@@ -2,42 +2,45 @@ import os
 import tamagotchi
 import threading
 import time
-
-# Función paso de tiempo y aumento de hambre, etc...
+opcion = ""
+# Función para el paso del tiempo y aumento de hambre, etc.
 def paso_tiempo():
     while True:
-        time.sleep(5)
-        os.system('cls' if os.name == 'nt' else 'clear')
-        mascota.nivel_hambre += 1
-        mascota.nivel_energia -= 2
-        mascota.nivel_felicidad -= 1
-        print(f"Nombre de la mascota: {mascota.nombre}")
-        mascota.verificar_estado()
-        print("-----------------------")
-    
-        # Muestra las opciones al usuario
-        print("Seleccione una opción:")
-        print("1. Alimentar")
-        print("2. Jugar")
-        print("3. Dormir")
-        print("4. Salir")
+        global opcion
+        if opcion == "4": break
+        else:
+            time.sleep(5)
+            os.system('cls' if os.name == 'nt' else 'clear')
+            mascota.nivel_hambre += 1
+            mascota.nivel_energia -= 2
+            mascota.nivel_felicidad -= 1
+            print(f"Nombre de la mascota: {mascota.nombre}")
+            mascota.verificar_estado()
+            print("-----------------------")
+
+            # Mostrar opciones al usuario
+            print("Seleccione una opción:")
+            print("1. Alimentar")
+            print("2. Jugar")
+            print("3. Dormir")
+            print("4. Salir")
 
 # Variable para verificar si el hilo ya ha sido iniciado
 hilo_iniciado = False
 
-# Limpia la consola
+# Limpiar la consola
 os.system('cls' if os.name == 'nt' else 'clear')
 
-# Muestra el mensaje de bienvenida
+# Mostrar mensaje de bienvenida
 print("Bienvenido al juego de Tamagotchi")
 print("----------------------------------")
 input("Presione Enter para comenzar...")
 nombre = input("Ingrese el nombre para su mascota: ")
 
-# Crea una instancia de la clase Tamagotchi
+# Crear una instancia de la clase Tamagotchi
 mascota = tamagotchi.Tamagotchi(nombre)
 
-# Crea un hilo para el tiempo
+# Crear un hilo para el tiempo
 hilo_aumentar_hambre = threading.Thread(target=paso_tiempo)
 
 # Función para iniciar el hilo
@@ -47,32 +50,29 @@ def start_thread():
         hilo_aumentar_hambre.start()
         hilo_iniciado = True
 
-# Inicia el hilo
-start_thread()
+# Iniciar el hilo
 
+start_thread()
 # Bucle principal del juego
 while True:
-    # Limpia la consola
+    # Limpiar la consola
     os.system('cls' if os.name == 'nt' else 'clear')
-    
-    # Inicia el hilo
-    start_thread()
     
     print(f"Nombre de la mascota: {mascota.nombre}")
     mascota.verificar_estado()
     print("-----------------------")
     
-    # Muestra las opciones al usuario
+    # Mostrar opciones al usuario
     print("Seleccione una opción:")
     print("1. Alimentar")
     print("2. Jugar")
     print("3. Dormir")
     print("4. Salir")
     
-    # Obtiene la opción seleccionada por el usuario
+    # Obtener la opción seleccionada por el usuario
     opcion = input()
     
-    # Realiza la acción seleccionada por el usuario
+    # Realizar la acción seleccionada por el usuario
     if opcion == "1":
         mascota.alimentar()
     elif opcion == "2":
@@ -80,7 +80,11 @@ while True:
     elif opcion == "3":
         mascota.dormir()
     elif opcion == "4":
+        # Salir del juego cuando se selecciona la opción 4
         hilo_aumentar_hambre.join()
+        os.system('cls' if os.name == 'nt' else 'clear')
         break
     else:
         print("Opción inválida")
+hilo_aumentar_hambre.join()
+os.system('cls' if os.name == 'nt' else 'clear')
--- Actividades/POO/Tamagotchi/tamagotchi.py
+++ Actividades/POO/Tamagotchi/tamagotchi.py
@@ -1,44 +1,51 @@
-import time
 import os
 class Tamagotchi:
-    nivel_energia=100   
-    nivel_hambre=0
-    nivel_felicidad=50
-    esta_vivo=True
     def __init__(self, nombre):
-        self.nombre=nombre
-        
-    
+        self.nombre = nombre
+        self.nivel_energia = 100  # Energía inicial en 100
+        self.nivel_hambre = 0  # Hambre inicial en 0
+        self.nivel_felicidad = 50  # Felicidad inicial en 50
+        self.esta_vivo = True
+
     def mostrar_estado(self):
         print(f'Energia:{self.nivel_energia}\n Hambre:{self.nivel_hambre}\n Felicidad:{self.nivel_felicidad}')
-    
+
     def verificar_estado(self):
         if self.nivel_energia <= 0:
             self.esta_vivo = False
             print(f'{self.nombre} ha muerto de cansancio')
+            input('Presione Enter para cerrar el programa.')
+            opcion = '4'
+            os.system('cls' if os.name == 'nt' else 'clear')
         elif self.nivel_hambre >= 100:
             self.esta_vivo = False
             print(f'{self.nombre} ha muerto de hambre')
-        elif self.nivel_felicidad<=0:
+            input('Presione Enter para cerrar el programa.')
+            opcion = '4'
+            os.system('cls' if os.name == 'nt' else 'clear')
+        elif self.nivel_felicidad <= 0:
             print(f'{self.nombre} ha muerto de tristeza')
+            input('Presione Enter para cerrar el programa.')
+            opcion = '4'
+            os.system('cls' if os.name == 'nt' else 'clear')
         else:
             self.mostrar_estado()
-        
+
     def alimentar(self):
-        self.nivel_hambre-=10
-        self.nivel_energia-=15
+        self.nivel_hambre = max(0, self.nivel_hambre - 10)  # Restringe el valor de hambre a un mínimo de 0
+        self.nivel_energia = max(0, self.nivel_energia - 15)  # Restringe el valor de energía a un mínimo de 0
         print(f'{self.nombre} ha comido')
-    
+
     def jugar(self):
-        self.nivel_felicidad+=20
-        self.nivel_energia-=18
-        self.nivel_hambre+=10
+        self.nivel_felicidad = min(100, self.nivel_felicidad + 20)  # Restringe el valor de felicidad a un máximo de 100
+        self.nivel_energia = max(0, self.nivel_energia - 18)  # Restringe el valor de energía a un mínimo de 0
+        self.nivel_hambre = min(100, self.nivel_hambre + 10)  # Restringe el valor de hambre a un máximo de 100
         print(f'{self.nombre} ha jugado')
-    
+
     def dormir(self):
-        self.nivel_energia+=40
-        self.nivel_hambre+=5
-        self.nivel_felicidad-=10
+        self.nivel_energia = min(100, self.nivel_energia + 40)  # Restringe el valor de energía a un máximo de 100
+        self.nivel_hambre = min(100, self.nivel_hambre + 5)  # Restringe el valor de hambre a un máximo de 100
+        self.nivel_felicidad = max(0, self.nivel_felicidad - 10)  # Restringe el valor de felicidad a un mínimo de 0
         print(f'{self.nombre} ha dormido')
 
 
